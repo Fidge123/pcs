7f0d3a014e3b4196b2a9b0027f5d3391c5d9b97f
Fidge123 <florian@lkr24.de>
2014-04-02 19:55:31 +0200

    setParmesan war doppelt

--- pesto.h
+++ pesto.h
@@ -3,5 +3,22 @@
 class Pesto
 {
 	public:
-		void setParmesan(int num);
-		void setPinienkerne(int num);		void setSalzUndPfeffer(int num);		void setKnoblauch(int num);		void setGetrockneteTomaten(int num);		void addWalnuss(int num);		void addParmesan(int num);		void addRucola(int num);		void addCashew(int num);		void setBasilikum(int num);		void addOlivenöl(int num);		void pürierePesto();		bool doesItLookLikePesto();		std::vector<Ingredients> getBaseIngredients();		//std::vector<Ingredients> getExtraIngredients();		std::vector<int> getExtraIngredients();	private:		struct Ingredients {};};
+		void setPinienkerne(int num);
+		void setSalzUndPfeffer(int num);
+		void setKnoblauch(int num);
+		void setGetrockneteTomaten(int num);
+		void addWalnuss(int num);
+		void addParmesan(int num);
+		void addRucola(int num);
+		void addCashew(int num);
+		void setBasilikum(int num);
+		void addOlivenï¿½l(int num);
+		void pï¿½rierePesto();
+		bool doesItLookLikePesto();
+		std::vector<Ingredients> getBaseIngredients();
+		//std::vector<Ingredients> getExtraIngredients();
+		std::vector<int> getExtraIngredients();
+
+	private:
+		struct Ingredients {};
+};
