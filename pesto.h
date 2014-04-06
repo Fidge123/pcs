dd1db34d63fe99faca84c74f02bb0ab80afceedf
florian.richter <florian@lkr24.de>
2014-04-06 21:26:07 +0200

    Final?

--- pesto.h
+++ pesto.h
@@ -3,24 +3,24 @@
 class Pesto
 {
 	public:
-		void setParmesan(int num);
-		void setPinienkerne(int num);
-		void setSalzUndPfeffer(int num);
-		void setKnoblauch(int num);
-		void setGetrockneteTomaten(int num);
-		void addWalnuss(int num);
-		void addParmesan(int num);
-		void addRucola(int num);
-		void addCashew(int num);
-		void setBasilikum(int num);
-		void addOlivenöl(int num);
-		void pürierePesto();
+		void setParmesan(int num, Bowl *bowl);
+		void setPinienkerne(int num, Bowl *bowl);
+		void setSalzUndPfeffer(int num, Bowl *bowl);
+		void setKnoblauch(int num, Bowl *bowl);
+		void setGetrockneteTomaten(int num, Bowl *bowl);
+		void addWalnuss(int num, Bowl *bowl);
+		void addParmesan(int num, Bowl *bowl);
+		void addRucola(int num, Bowl *bowl);
+		void addCashew(int num, Bowl *bowl);
+		void setBasilikum(int num, Bowl *bowl);
+		void addOlivenöl(int num, Bowl *bowl);
+		void pürierePesto(Bowl *bowl);
 		bool doesItLookLikePesto();
 		std::vector<Ingredients> getBaseIngredients();
 		std::vector<Ingredients> getExtraIngredients();
-		Pestoglass konservierePesto();
+		GlassOfPesto konservierePesto();
 
 	private:
 		struct Ingredients {};
-		struct Pestoglass {};
+		struct GlassOfPesto {};
 };
--- pestocode.cpp
+++ pestocode.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
+struct GlassOfPesto{};
+
 struct Rezept{
 	vector<Ingredients> getBaseIngredients();
 	vector<Ingredients> getExtraIngredients();
 };
-
-struct Pestoglass{};
 
 struct Bowl{
 	void clean(){
@@ -23,7 +23,6 @@ struct RGB {
 	short R, G, B;
 };
 
-
 RGB getRGBColor(Rezept* myPesto){
 	RGB color = {255, 255, 0};
 	return color;
@@ -33,75 +32,105 @@ int getNumberOfServings(){
 	return 1;
 }
 
-void setBaseIngredients(Pesto* pesto, int servings){
-	pesto->setParmesan(75 * servings);
-	pesto->setPinienkerne(50 * servings);
-	pesto->setSalzUndPfeffer(servings);
-	pesto->setKnoblauch(2 * servings);
-}
-
-void createPestoRosso(Pesto* pesto, int servings, vector<Ingredients> extras){
-	pesto->setGetrockneteTomaten(125 * servings);
-	for(auto iterator : extras){
-		if(iterator.name == "Walnuss")
-			pesto->addWalnuss(100 * servings);
-		if(iterator.name == "Parmesan")
-			pesto->addParmesan(25 * servings);
-	}
-}
-
-void createPestoVerde(Pesto* pesto, int servings, vector<Ingredients> extras){
-	pesto->setBasilikum(5 * servings);
-	for(auto iterator : extras){
-		if(iterator.name == "Rucola")
-			pesto->addRucola(50 * servings);
-		if(iterator.name == "Cashew")
-			pesto->addCashew(50 * servings);
-		if(iterator.name == "Parmesan")
-			pesto->addParmesan(25 * servings);
-	}
-}
-
 bool isPurchased(vector<Ingredients> all, vector<Ingredients> purchasedStuff){
 	return true;
 }
 
 void buyIngredients(){
-
 }
 
-Pestoglass createPesto(vector<Ingredients> purchasedStuff, Rezept* myPesto){
+bool looksNotPesto(){
+	return false;
+}
+
+// VS2012 does not support user-defined literals yet
+// feel free to correct this part as I didnt test it
+
+/**
+constexpr Units operator "" _g(int g)
+{
+  return Units{Units::IntIsGramm{}, static_cast<int>(g)};
+}
+
+constexpr Units operator "" _EL(int el)
+{
+  return Units{Units::IntIsEL{}, static_cast<int>(el)};
+}
+
+constexpr Units operator "" _zehen(int z)
+{
+  return Units{Units::IntIsZehen{}, static_cast<int>(z)};
+}
+
+constexpr Units operator "" _blätter(int b)
+{
+  return Units{Units::IntIsBlätter{}, static_cast<int>(b)};
+}
+**/
+
+// the interesting stuff begins here
+void setBaseIngredients(Pesto* pesto, int servings, Bowl *bowl){
+	pesto->setParmesan(75_g * servings, bowl);
+	pesto->setPinienkerne(50_g * servings, bowl);
+	pesto->setSalzUndPfeffer(servings, bowl);
+	pesto->setKnoblauch(2_zehen * servings, bowl);
+}
+
+void createPestoRosso(Pesto* pesto, int servings, vector<Ingredients> extras, Bowl *bowl){
+	pesto->setGetrockneteTomaten(125_g * servings, bowl);
+	for(auto iterator : extras){
+		if(iterator.name == "Walnuss")
+			pesto->addWalnuss(100_g * servings, bowl);
+		if(iterator.name == "Parmesan")
+			pesto->addParmesan(25_g * , bowl);
+	}
+}
+
+void createPestoVerde(Pesto* pesto, int servings, vector<Ingredients> extras, Bowl *bowl){
+	pesto->setBasilikum(5_blätter * servings, bowl);
+	for(auto iterator : extras){
+		if(iterator.name == "Rucola")
+			pesto->addRucola(50_g * servings, bowl);
+		if(iterator.name == "Cashew")
+			pesto->addCashew(50_g * servings, bowl);
+		if(iterator.name == "Parmesan")
+			pesto->addParmesan(25_g * servings, bowl);
+	}
+}
+
+GlassOfPesto createPesto(vector<Ingredients> purchasedStuff, Rezept* myPesto){
 	Bowl *bowl = new Bowl();
-	bool texture;
+	bool texture = looksNotPesto();
 	RGB myColor = getRGBColor(myPesto);
 	RGB rosso = {255, 0, 0};
 	RGB verde = {0, 255, 0};
 	int servings = getNumberOfServings();
+
+	Pesto *pesto = new Pesto();
 
 	vector<Ingredients> base = myPesto->getBaseIngredients();
 	vector<Ingredients> extras = myPesto->getExtraIngredients();
 	vector<Ingredients> all = base;
 	all.insert(all.end(), extras.begin(), extras.end());
 
-	Pesto *pesto = new Pesto();
-
 	if(!isPurchased(all, purchasedStuff))
 		buyIngredients();
 	
-	setBaseIngredients(pesto, servings); 
+	setBaseIngredients(pesto, servings, bowl); 
 	if(myColor.R == rosso.R)
-		createPestoRosso(pesto, servings, extras);
+		createPestoRosso(pesto, servings, extras, bowl);
 	if(myColor.G == verde.G)
-		createPestoVerde(pesto, servings, extras);
+		createPestoVerde(pesto, servings, extras, bowl);
 
 	while(!texture){
-		pesto->addOlivenöl(3 * servings);
-		pesto->pürierePesto();
+		pesto->addOlivenöl(3_EL * servings);
+		pesto->pürierePesto(bowl);
 		texture = pesto->doesItLookLikePesto();
 	}
 	
-	Pestoglass glass = pesto->konservierePesto();
+	GlassOfPesto glass = pesto->konservierePesto();
 	bowl->clean();
-	// schmeckt am besten zu nudeln oder baguette
+
+	// schmeckt gut zu Nudeln oder Baguette
 	return glass;
 }
